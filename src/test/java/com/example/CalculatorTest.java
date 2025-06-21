0e5c2fd6fd6708db21acddb135ab6b1fca99a90e
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-06-21 02:02:28 -0500

    feat: Add subtract method and tests

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -9,4 +9,10 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3), "2 + 3 should be 5");
     }
+
+    @Test
+    void testSubtract() { // Nuevo test
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.subtract(3, 2), "3 - 2 should be 1");
+    }
 }
